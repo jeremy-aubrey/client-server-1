7e8ae20e33e83024cc41d80e66938e31eedbf8c9
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-16 14:29:02 -0600

    connects / recieves date from server

--- src/QuoteClient.java
+++ src/QuoteClient.java
@@ -18,6 +18,9 @@
 //
 //********************************************************************
 
+import java.net.*;
+import java.io.*;
+
 public class QuoteClient
 {
     //***************************************************************
@@ -37,6 +40,23 @@ public class QuoteClient
 		// the non-static developerInfo and other non-static methods
 		QuoteClient client = new QuoteClient();
 		client.developerInfo();
+		
+		try {
+			/* make connection to server socket */
+			Socket sock = new Socket("127.0.0.1",6013);
+			InputStream in = sock.getInputStream();
+			BufferedReader bin = new
+			BufferedReader(new InputStreamReader(in));
+			/* read the date from the socket */
+			String line;
+			while ( (line = bin.readLine()) != null)
+			System.out.println(line);
+			/* close the socket connection*/
+			sock.close();
+			}
+			catch (IOException ioe) {
+			System.err.println(ioe);
+			}
 
 	} // End of the main method
 	
